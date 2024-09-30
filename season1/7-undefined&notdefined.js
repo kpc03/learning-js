37b817f4ec41d326f712e030e25ab88bafaeacc1
kanchaan chopraa <kanchaanchopraa@gmail.com>
2024-09-30 20:00:59 +0530

    episode 7 done

---
+++ season1/7-undefined&notdefined.js
@@ -0,0 +1,16 @@
+var a;
+console.log(a);
+
+a = 10;
+console.log(a);
+
+a = "Hello World";
+console.log(a);
+
+// undefined - undefined is one the primitive data type or you can also call it a reserved keyword which is being used by JS engine whenever a variable is first initialised in execution context in phase 1. It also means that the variable is having some space in memory of execution context.
+
+// not defined - no memory has been allocated and the variable is called. Its an error.
+
+// JS is loosely typed language which means it doesn't specify any avriablee to its data type. Above small code is the example of that where a is assigned multiple data type values and this is valid javascript.
+
+// a = undefined - never assign undefines to any variable as its not a good practice.
