fdc08feb10cdebb61bd24879261190f377b7caaf
kanchaan chopraa <kanchaanchopraa@gmail.com>
2025-01-12 14:26:52 +0530

    forEach demo and variable shadowing

---
+++ season1/forEachDemo.js
@@ -0,0 +1,12 @@
+// Demo of forEach loop and also understood variable shadowing
+
+const number = [12,23,34,45,56,67,78,89];
+number.forEach(function(number, index){
+    console.log(`${number} ${index}`);
+    console.log(number.length);
+});
+
+number.forEach(function(item){
+    console.log(item);
+    console.log(number.length);
+});
